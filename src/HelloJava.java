194447173d92628519b444c4d74ac3eba8f5e777
cli669 <cli669@M3084184.Go.JohnsonControls.com>
2018-09-03 11:47:49 +0800

    提交项目

---
+++ src/HelloJava.java
@@ -0,0 +1,10 @@
+
+public class HelloJava {
+private static String say="woyaoxueni";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ddfdsfd"+say);
+		System.out.println("qqq"+say);
+	}
+
+}
